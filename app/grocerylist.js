12a816dbccfe32aea512ff2db6958252fbb9bbbf
seeker8 <seeker8@users.noreply.github.com>
2017-07-13 23:22:17 -0500

    Demonstrate the usage of props.children

--- app/grocerylist.js
+++ app/grocerylist.js
@@ -4,9 +4,9 @@ import ListItem from './listitem';
 export default class GroceryList extends React.Component {
   render() {
     return (<ul>
-      <ListItem quantity="1" name="Bread" />
-      <ListItem quantity="6" name="Eggs" />
-      <ListItem quantity="2" name="Milk" />
+      <ListItem quantity="1">Bread </ListItem>
+      <ListItem quantity="6">Eggs</ListItem>
+      <ListItem quantity="2">Milk</ListItem>
     </ul>);
   }
 }
--- app/listitem.js
+++ app/listitem.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class ListItem extends React.Component {
   render() {
     return (
-      <li> {this.props.quantity} x {this.props.name}</li>
+      <li> {this.props.quantity} x {this.props.children}</li>
     );
   }
 }
